ddb93ee5f4fde5581e5b0bfaf686f5d36316e26d
pegaislethal <ghimirerohit21@gmail.com>
2026-01-10 10:44:24 +0545

    Cleaned Index.js for backend

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-const bodyParser = require("body-parser");
 const mainRouter = require("./routes/index.route");
 const { swaggerUi, swaggerSpec } = require("./config/swagger");
-// Initialize Cloudinary configuration
 require("./config/cloudinary");
-
-// const session = require("express-session");
-// const passport = require("passport");
 const cors = require("cors");
 
 const app = express();
@@ -22,20 +17,15 @@ app.use(
       "http://localhost:3001",
       "http://localhost:3002",
       "http://localhost:3003",
-      "https://financefusion-six.vercel.app",
     ],
     credentials: true,
   })
 );
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", mainRouter);
-app.get("/", (req, res) => {
-  res.send("MedTrack App");
-});
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
