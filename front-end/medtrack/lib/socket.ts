4aae84de2f66ea17a73fb7c032f163dd78041e36
pegaislethal <ghimirerohit21@gmail.com>
2026-05-07 16:57:06 +0545

    (update)- update api based url

--- front-end/medtrack/lib/socket.ts
+++ front-end/medtrack/lib/socket.ts
@@ -4,18 +4,18 @@ let socket: Socket | null = null;
 
 const getSocketBaseUrl = (): string =>
   (() => {
-    const localSocketBaseUrl = "http://localhost:5000";
-    const isLocalFrontend =
-      typeof window !== "undefined" &&
-      (window.location.hostname === "localhost" ||
-        window.location.hostname === "127.0.0.1");
+    // const localSocketBaseUrl = "";
+    // const isLocalFrontend =
+    //   typeof window !== "undefined" &&
+    //   (window.location.hostname === "localhost" ||
+    //     window.location.hostname === "127.0.0.1");
 
     const apiBase =
       process.env.NEXT_PUBLIC_API_URL ||
-      "https://medtrack-2t04.onrender.com/api";
+      "";
 
     const prodSocketBaseUrl = apiBase.replace(/\/api$/, "");
-    return isLocalFrontend ? localSocketBaseUrl : prodSocketBaseUrl;
+    return prodSocketBaseUrl;
   })();
 
 export const getSocket = (): Socket => {
